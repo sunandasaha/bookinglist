e14507dd62ebf64cdd5091a0b4df1cebd8264633
vidyavandana <vidyavandana1234@gmail.com>
2025-06-17 18:18:41 +0530

    badge icon

--- Front/src/app/calendar/components/TopBar.jsx
+++ Front/src/app/calendar/components/TopBar.jsx
@@ -236,6 +236,12 @@ export default function TopBar({ selectedDate, setSelectedDate, searchBID, setSe
               size={24}
               title="Notifications"
             />
+            {pending && pending.length >0 && (
+              <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                {pending.length}
+              </span>
+            )}
+             
           </div>
         </div>
 
