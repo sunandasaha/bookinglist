de3eb4dd3bde4e4e7f7fb357385b85acf3b2c10f
vidyavandana <vidyavandana1234@gmail.com>
2025-07-05 12:40:29 +0530

    .

--- Front/src/app/_utils/request.ts
+++ Front/src/app/_utils/request.ts
@@ -1,4 +1,4 @@
-const site = "http://localhost:8080/";
+const site = "https://bookinglist-API.onrender.com/";
 const postReq = async (surl: string, data: {}, tok: string) => {
   const res = await fetch(site + surl, {
     method: "POST",
