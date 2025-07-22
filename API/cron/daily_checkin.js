b9579f84bf1db1a93dad8d766153b239ab88f085
Bapiya <bapiya1@gmail.com>
2025-07-22 09:31:08 +0530

    small fix

--- API/cron/daily_checkin.js
+++ API/cron/daily_checkin.js
@@ -6,10 +6,4 @@ const dailyCheckin = async (req, res) => {
   res.json({ success: true });
 };
 
-// const testCron = async (req, res) => {
-//   const bok = await GuestModel.findById("BK3104653316530");
-//   sendEmail(bok);
-//   res.json({ success: true });
-// };
-
 module.exports = { dailyCheckin };
