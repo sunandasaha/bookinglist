0951a8c9febfe05f21bfcfd440201cd3fe0e7d57
Bapiya <bapiya1@gmail.com>
2025-07-28 20:18:56 +0530

    sync

--- API/cron/daily_checkin.js
+++ API/cron/daily_checkin.js
@@ -9,10 +9,11 @@ const dailyCheckin = async (req, res) => {
 const dailyChkOut = async (req, res) => {
   date = new Date(new Date().toISOString().substring(0, 10));
   const bookings = await GuestModel.find({
-    status: { $in: [1, 11] },
+    status: 11,
     toDate: { $lte: date },
-  });
-  res.json({ success: true });
+    advance_ss: { $exists: true },
+  }).select("advance_ss");
+  res.json({ success: true, bookings });
 };
 
 module.exports = { dailyCheckin };
